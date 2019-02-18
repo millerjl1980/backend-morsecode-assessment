c1601cffd4fb3daf110ffea4a521bae54e0d0f02
Piero Madar <madarp@pobox.com>
2019-02-18 16:05:57 -0500

    PEP8 on starter code

--- morse.py
+++ morse.py
@@ -14,23 +14,21 @@ When transmitting the Morse code, the international standard specifies that:
 Pause between dots and dashes in a character – is 1 time unit long.
 Pause between characters inside a word – is 3 time units long.
 Pause between words – is 7 time units long.
-However, the standard does not specify how long that "time unit" is. 
-And in fact different 
-operators would transmit at different speed. An amateur person may need a few seconds to 
-transmit a single character, a skilled professional can transmit 60 words per minute, 
+However, the standard does not specify how long that "time unit" is.
+And in fact different
+operators would transmit at different speed. An amateur person may need a few seconds to
+transmit a single character, a skilled professional can transmit 60 words per minute,
 and robotic transmitters may go way faster.
 
 OK FOR REAL??  60 WPM??
 https://morsecode.scphillips.com/translator.html
 
-For this kata we assume the message receiving is performed automatically by the hardware 
-that checks the line periodically, and if the line is connected (the key at the remote 
-station is down), 1 is recorded, and if the line is not connected (remote key is up), 
-0 is recorded. After the message is fully received, it gets to you for decoding as a 
+For this kata we assume the message receiving is performed automatically by the hardware
+that checks the line periodically, and if the line is connected (the key at the remote
+station is down), 1 is recorded, and if the line is not connected (remote key is up),
+0 is recorded. After the message is fully received, it gets to you for decoding as a
 string containing only symbols 0 and 1.
-
 """
-import re
 
 # This dictionary is supplied within the Codewars test suite.
 MORSE_CODE = {
